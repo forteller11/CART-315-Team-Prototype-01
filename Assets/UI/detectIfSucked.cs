033003070ac315f4e9e1e2ed6c111195b15d7656
forteller11 <charly.yan.miller@gmail.com>
2020-02-29 23:10:42 -0500

    merged changes

---
+++ Assets/UI/detectIfSucked.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class detectIfSucked : MonoBehaviour
+{
+    public bool obj0Sucked;
+    public bool obj1Sucked;
+    public bool obj2Sucked;
+    public bool obj3Sucked;
+    public bool obj4Sucked;
+    public bool obj5Sucked;
+
+    private Image obj0;
+    private Image obj1;
+    private Image obj2;
+    private Image obj3;
+    private Image obj4;
+    private Image obj5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj0Sucked = false;
+        obj1Sucked = false;
+        obj2Sucked = false;
+        obj3Sucked = false;
+        obj4Sucked = false;
+        obj5Sucked = false;
+
+        obj0 = GameObject.Find("obj 0").GetComponent<Image>();
+        obj1 = GameObject.Find("obj 1").GetComponent<Image>();
+        obj2 = GameObject.Find("obj 2").GetComponent<Image>();
+        obj3 = GameObject.Find("obj 3").GetComponent<Image>();
+        obj4 = GameObject.Find("obj 4").GetComponent<Image>();
+        obj5 = GameObject.Find("obj 5").GetComponent<Image>();
+
+        var startColor = obj0.color;
+        startColor.a = 0.25f;
+        obj0.color = startColor;
+        obj1.color = startColor;
+        obj2.color = startColor;
+        obj3.color = startColor;
+        obj4.color = startColor;
+        obj5.color = startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        var currentColor = obj0.color;
+        if(obj0Sucked == true)
+        {
+            currentColor.a = 1f;
+        } else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj0.color = currentColor;
+
+        if (obj1Sucked == true)
+        {
+            currentColor.a = 1f;
+        }
+        else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj1.color = currentColor;
+
+        if (obj2Sucked == true)
+        {
+            currentColor.a = 1f;
+        }
+        else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj2.color = currentColor;
+
+        if (obj3Sucked == true)
+        {
+            currentColor.a = 1f;
+        }
+        else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj3.color = currentColor;
+
+        if (obj4Sucked == true)
+        {
+            currentColor.a = 1f;
+        }
+        else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj4.color = currentColor;
+
+        if (obj5Sucked == true)
+        {
+            currentColor.a = 1f;
+        }
+        else
+        {
+            currentColor.a = 0.25f;
+        }
+        obj5.color = currentColor;
+    }
+}
---
+++ Assets/UI/moveHighlight.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveHighlight : MonoBehaviour
+{
+    RectTransform UIContainer;
+    Vector3 startingPosition;
+
+    public int index;
+
+    private const int INDEX_CEILING = 6;
+    public const float X_TRANS_INTERVAL = 3.85f;
+
+    private bool buttonIsBeingPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIContainer = GameObject.Find("UI").GetComponent<RectTransform>();
+        startingPosition = new Vector3(0f + UIContainer.position.x, 0f + UIContainer.position.y, 0f + UIContainer.position.z);
+        transform.position = startingPosition;
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cycle"))
+        {
+            index++;
+            index = index % INDEX_CEILING;
+            Debug.Log(index);
+            buttonIsBeingPressed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (buttonIsBeingPressed == true)
+        {
+            Debug.Log("updating ui...");
+            UpdatePosition();
+            buttonIsBeingPressed = false;
+        }
+        //transform.Translate(0f, -0.001f, 0f);
+        // translate 1
+    }
+
+    private void UpdatePosition()
+    {
+        switch (index)
+        {
+            case 0:
+                Debug.Log("item 0");
+                transform.Translate(-X_TRANS_INTERVAL * (INDEX_CEILING - 1), 0f, 0f);
+                break;
+            case 1:
+                Debug.Log("item 1");
+                transform.Translate(X_TRANS_INTERVAL, 0f, 0f);
+                break;
+            case 2:
+                Debug.Log("item 2");
+                transform.Translate(X_TRANS_INTERVAL, 0f, 0f);
+                break;
+            case 3:
+                Debug.Log("item 3");
+                transform.Translate(X_TRANS_INTERVAL, 0f, 0f);
+                break;
+            case 4:
+                Debug.Log("item 4");
+                transform.Translate(X_TRANS_INTERVAL, 0f, 0f);
+                break;
+            case 5:
+                Debug.Log("item 5");
+                transform.Translate(X_TRANS_INTERVAL, 0f, 0f);
+                break;
+        }
+    }
+}
