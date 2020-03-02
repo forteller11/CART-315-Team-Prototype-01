6837f266eb1a2c5b78035d139ceb3b90d0120fd8
forteller11 <charly.yan.miller@gmail.com>
2020-03-02 05:31:21 -0500

    cleaned up code

--- Assets/Scripts/Vaccum/Swallowable.cs
+++ Assets/Scripts/Vaccum/Swallowable.cs
@@ -7,22 +7,20 @@ using UnityEngine;
 
 public class Swallowable : MonoBehaviour, IPoolable
 {
-    public SuckableType Type = SuckableType.NonCombinable;
+    public CombinableType Type;
     public GameObjectPool Pool { get; set; }
     [HideInInspector] public bool BeginCheckingForCombinations;
     [Flags]
-    public enum SuckableType
+    public enum CombinableType
     {
-        NonCombinable = 0b_0000_0001,
-        Energy        = 0b_0000_0010,
-        Liquid        = 0b_0000_0100,
-        Clothes       = 0b_0000_1000
+        Energy        = 0b_0000_0001,
+        Liquid        = 0b_0000_0010,
+        Clothes       = 0b_0000_0100
     }
 
     [HideInInspector] public Rigidbody2D Rigidbody2D;
 
     private void Awake() => Rigidbody2D = GetComponent<Rigidbody2D>();
-    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -30,22 +28,24 @@ public class Swallowable : MonoBehaviour, IPoolable
             return;
 
         var otherSwallowable = other.GetComponent<Swallowable>();
-        if (otherSwallowable == null)
+        if (otherSwallowable == null) //make sure other item is of combinable/swalloable type
             return;
 
         var combinedType = otherSwallowable.Type | Type;
 
-        //call methods in body approrpaite power ups
+        //Add Functionality here
         switch (combinedType)
         {
-            case SuckableType.Liquid | SuckableType.Energy:
+            //if a liquid and energy type combine...
+            case CombinableType.Liquid | CombinableType.Energy:
                 VaccumBodySingleton.Instance.GetComponent<SpriteRenderer>().color = Color.red;
                 this.Pool.ReturnToPool(this.gameObject);
                 otherSwallowable.Pool.ReturnToPool(otherSwallowable.gameObject);
-                
-                //start coroutine to force multiplier in settings....
-                
-                //at end it changes it back
+                //TODO start coroutine to force multiplier in settings.... at end it changes it back
+                break;
+            
+            //if a clothe and energy type combine...
+            case CombinableType.Clothes | CombinableType.Energy:
                 break;
         }
         
