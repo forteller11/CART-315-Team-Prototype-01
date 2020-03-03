541666712edd1dedbb72e5e4a019c8ff22f62cc8
mortebouse <31903560+mortebouse@users.noreply.github.com>
2020-03-03 14:40:52 -0500

    made a toggleable ui overlay that displays all controls since we have a lot
    some of the inputs have yet to be determined and also i need to make it more eye-catching for greater clarity. i'll do those things a bit later

---
+++ Assets/Scripts/Vaccum/viewControlsMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class viewControlsMenu : MonoBehaviour
+{
+    private Text menuInstructionText;
+    private GameObject menuOverlay;
+    private bool menuToggled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuInstructionText = GameObject.Find("Menu Instruction Text").GetComponent<Text>();
+        menuOverlay = GameObject.Find("Menu Overlay");
+        menuToggled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("View Menu"))
+        {
+            menuToggled = !menuToggled;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (menuToggled == false)
+        {
+            menuOverlay.SetActive(false);
+            menuInstructionText.text = "[Start] / [Esc] - Controls ";
+
+        } else
+        {
+            menuOverlay.SetActive(true);
+            menuInstructionText.text = "[Start] / [Esc] - Return to game ";
+        }
+    }
+}
