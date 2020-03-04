004fdf1d87f13ef3088f89bdfdae75f2bae261f6
mortebouse <31903560+mortebouse@users.noreply.github.com>
2020-03-03 23:44:19 -0500

    had to re-edit something after a merge conflict

--- Assets/Scripts/Vaccum/CombineManager.cs
+++ Assets/Scripts/Vaccum/CombineManager.cs
@@ -79,6 +79,9 @@ namespace Legs
         {
             yield return new WaitForSeconds(SuckIncreaseTimeOnEnergyCollision);
             SuckObjects.PowerUpSuckMultiplier = 1f;
+
+            notificationText.text = "Suck power returned to normal";
+            Debug.Log("back to normal");
         }
     }
 }
